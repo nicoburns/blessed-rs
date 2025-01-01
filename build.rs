822b042203350f29e8642f2579ba29a35da3bea3
Nico Burns <nico@nicoburns.com>
2025-01-02 01:14:12 +1300

    Switch to grass for sass compilation
    Signed-off-by: Nico Burns <nico@nicoburns.com>

--- build.rs
+++ build.rs
@@ -1,9 +1,8 @@
-use std::path::PathBuf;
-use rsass::{compile_scss_path, output::{Format, Style}};
 use std::fs;
+use std::path::PathBuf;
 
 fn get_input_path(filename: &str) -> PathBuf {
-  let mut path = PathBuf::with_capacity(50);
+    let mut path = PathBuf::with_capacity(50);
     path.push(".");
     path.push("styles");
     path.push(filename);
@@ -12,7 +11,7 @@ fn get_input_path(filename: &str) -> PathBuf {
 }
 
 fn get_output_path(filename: &str) -> PathBuf {
-  let mut path = PathBuf::with_capacity(50);
+    let mut path = PathBuf::with_capacity(50);
     path.push(".");
     path.push("static");
     path.push(filename);
@@ -20,24 +19,15 @@ fn get_output_path(filename: &str) -> PathBuf {
     path
 }
 
-fn main () {
-  println!("cargo:rerun-if-changed=styles");
+fn main() {
+    println!("cargo:rerun-if-changed=styles");
 
+    let entrypoints = ["index"];
+    for filename in &entrypoints {
+        let input_path = get_input_path(filename);
+        let css = grass::from_path(&input_path, &grass::Options::default()).unwrap();
 
-  let entrypoints = ["index"];
-  for filename in &entrypoints {
-    
-    let input_path = get_input_path(filename);
-    let css = compile_scss_path(
-      &input_path,
-      Format {
-        style: Style::Expanded,
-        ..Default::default()
-      }
-    ).unwrap();
-
-    let output_path = get_output_path(filename);
-    fs::write(output_path, css).unwrap();
-
-  }
+        let output_path = get_output_path(filename);
+        fs::write(output_path, css).unwrap();
+    }
 }
