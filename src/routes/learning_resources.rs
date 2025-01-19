cd0225545ba7437fd2a932ebe5c3cea967921564
Nico Burns <nico@nicoburns.com>
2025-01-20 11:20:17 +1300

    Add more learning resources
    Signed-off-by: Nico Burns <nico@nicoburns.com>

--- src/routes/learning_resources.rs
+++ src/routes/learning_resources.rs
@@ -8,13 +8,18 @@ use crate::templates::{ TERA, TocSection, TocSubSection };
 pub(crate) async fn run() -> impl IntoResponse {
 
     let toc_sections = vec![
-        TocSection { name: "Books".into(), slug: "learning-resources".into(), subsections: vec![
+        TocSection { name: "Books".into(), slug: "books".into(), subsections: vec![
             TocSubSection { name: "Introductory Books".into(), slug: "introductory-books".into() },
             TocSubSection { name: "Advanced Books".into(), slug: "advanced-books".into() },
         ]},
-        TocSection { name: "Other".into(), slug: "learning-resources".into(), subsections: vec![
+        TocSection { name: "Other".into(), slug: "other".into(), subsections: vec![
             TocSubSection { name: "Learning by doing".into(), slug: "by-doing".into() },
+            TocSubSection { name: "Learning by example".into(), slug: "by-example".into() },
             TocSubSection { name: "Videos".into(), slug: "videos".into() },
+        ]},
+        TocSection { name: "Reference".into(), slug: "reference".into(), subsections: vec![
+            TocSubSection { name: "Official Documentation".into(), slug: "official-docs".into() },
+            TocSubSection { name: "Unofficial Documentation".into(), slug: "unofficial-docs".into() },
         ]},
         // TocSection { name: "Community".into(), slug: "community".into(), subsections: vec![
         //     TocSubSection { name: "Crate Discovery".into(), slug: "crate-discovery".into() },
