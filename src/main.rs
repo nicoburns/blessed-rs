150c6f12164bd81011dd878e85728979639ccd6e
Nico Burns <nico@nicoburns.com>
2025-03-06 12:03:22 +1300

    Cache rendered pages in-memory
    Signed-off-by: Nico Burns <nico@nicoburns.com>

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,19 @@
 use axum::{
+    body::Bytes,
     http::StatusCode,
     response::{Html, IntoResponse, Redirect},
     routing::{get, get_service},
     Router,
 };
+use dashmap::DashMap;
 use dioxus::prelude::*;
 use dioxus_html_macro::html;
 use routes::{CrateListPage, GettingStartedPage, LearningResourcesPage};
 use std::{
+    any::{Any, TypeId},
     net::{IpAddr, SocketAddr},
-    time::Instant,
+    sync::LazyLock,
+    time::{Duration, Instant},
 };
 use tokio::net::TcpListener;
 use tower_http::{services::ServeDir, trace::TraceLayer};
@@ -25,14 +29,17 @@ async fn main() {
     // build our application with a route
     let app = Router::new()
         .route("/", get(|| async { Redirect::to("/crates") }))
-        .route("/crates", get(|| dx_route(|| html!(<CrateListPage />))))
+        .route(
+            "/crates",
+            get(|| dx_route_cached(|| html!(<CrateListPage />))),
+        )
         .route(
             "/getting-started",
-            get(|| dx_route(|| html!(<GettingStartedPage />))),
+            get(|| dx_route_cached(|| html!(<GettingStartedPage />))),
         )
         .route(
             "/learning-resources",
-            get(|| dx_route(|| html!(<LearningResourcesPage />))),
+            get(|| dx_route_cached(|| html!(<LearningResourcesPage />))),
         )
         .nest_service("/static", get_service(ServeDir::new("static")))
         .layer(TraceLayer::new_for_http());
@@ -58,18 +65,40 @@ async fn main() {
         .unwrap();
 }
 
+async fn dx_route_cached(render_fn: fn() -> Element) -> impl IntoResponse {
+    static CACHE: LazyLock<DashMap<TypeId, Bytes>> = LazyLock::new(|| DashMap::new());
+
+    let type_id = Any::type_id(&render_fn);
+
+    let html = CACHE.entry(type_id).or_insert_with(|| {
+        let (html, duration) = render_component(render_fn);
+
+        let duration_millis = duration.as_micros() as f64 / 1000.0;
+        println!("Rendered in {duration_millis:.2}ms",);
+
+        Bytes::from(html)
+    });
+
+    (StatusCode::OK, Html(html.clone()))
+}
+
+#[allow(unused)]
 async fn dx_route(render_fn: fn() -> Element) -> impl IntoResponse {
+    let (html, duration) = render_component(render_fn);
+
+    let duration_millis = duration.as_micros() as f64 / 1000.0;
+    println!("Rendered dx in {duration_millis:.2}ms",);
+
+    (StatusCode::OK, Html(html))
+}
+
+fn render_component(render_fn: fn() -> Element) -> (String, Duration) {
     let start = Instant::now();
 
     let mut dom = VirtualDom::new(render_fn);
     dom.rebuild_in_place();
     let rendered = dioxus_ssr::render(&dom);
-    let rendered = format!("<!DOCTYPE html><html{}", &rendered[4..]);
+    let html = format!("<!DOCTYPE html><html{}", &rendered[4..]);
 
-    println!(
-        "Rendered dx in {:.2}ms",
-        start.elapsed().as_micros() as f64 / 1000.0
-    );
-
-    (StatusCode::OK, Html(rendered))
+    (html, start.elapsed())
 }
